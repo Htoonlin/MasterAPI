fe0fae7e66acf87f73e91d30252ea06815df46dc
htoonlin <wizard.htoon@gmail.com>
2017-02-15 14:05:50 +0630

    - Add BarcodeManager in Core/Utilities

---
+++ src/main/java/com/sdm/core/util/BarCodeManager.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sdm.core.util;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author Htoonlin
+ */
+public class BarCodeManager {
+
+    private static final String FILE_TYPE = "png";
+
+    private static void imageWriter(File outputFile, BitMatrix byteMatrix) throws IOException {
+        int matrixWidth = byteMatrix.getWidth();
+        int matrixHeight = byteMatrix.getHeight();
+        BufferedImage image = new BufferedImage(matrixWidth, matrixHeight, BufferedImage.TYPE_INT_RGB);
+        image.createGraphics();
+        Graphics2D graphics = (Graphics2D) image.getGraphics();
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, matrixWidth, matrixHeight);
+        graphics.setColor(Color.BLACK);
+        for (int i = 0; i < matrixWidth; i++) {
+            for (int j = 0; j < matrixHeight; j++) {
+                if (byteMatrix.get(i, j)) {
+                    graphics.fillRect(i, j, 1, 1);
+                }
+            }
+        }
+
+        ImageIO.write(image, FILE_TYPE, outputFile);
+    }
+
+    public static void createBarcode(File barcodeFile, BarcodeFormat format, String content, int width, int height) throws WriterException, IOException {
+        Hashtable hintMap = new Hashtable();
+        hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+        MultiFormatWriter codeWriter = new MultiFormatWriter();
+        BitMatrix byteMatrix = codeWriter.encode(content, format, width, height, hintMap);
+        imageWriter(barcodeFile, byteMatrix);
+    }
+
+
+    public static void createQR(File qrFile, String content, int size) throws WriterException, IOException {
+        Hashtable hintMap = new Hashtable();
+        hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+        QRCodeWriter codeWriter = new QRCodeWriter();
+        BitMatrix byteMatrix = codeWriter.encode(content, BarcodeFormat.QR_CODE, size, size, hintMap);
+        imageWriter(qrFile, byteMatrix);
+    }
+}
